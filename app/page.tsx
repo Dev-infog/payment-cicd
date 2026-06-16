c4d07c4a5a925d4e73026f653afdfddd69fdd206
Dev-infog <Dhanesvar@infogsolution.com>
2026-06-16 12:51:30 +0530

    payment page commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,81 @@
-import Image from "next/image";
+"use client";
 
 export default function Home() {
+  const courses = [
+  {
+    id: "plan_basic",
+    name: "Basic Plan",
+    price: 499,
+    features: "Access to basic tutorials",
+  },
+  {
+    id: "plan_premium",
+    name: "premium Plan",
+    price: 999,
+    features: "Access to Full course access",
+  },
+];
+
+  const handlePayment = async (price: number) => {
+    // 1. Create order
+    const res = await fetch("/api/create-order", {
+      method: "POST",
+      body: JSON.stringify({ amount: price }),
+    });
+
+    const order = await res.json();
+
+    // 2. Razorpay options
+    const options = {
+      key: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
+      amount: order.amount,
+      currency: "INR",
+      name: "Course Platform",
+      description: "Course Purchase",
+      order_id: order.id,
+      handler: async function (response: any) {
+        // 3. Verify payment
+        const verify = await fetch("/api/verify-payment", {
+          method: "POST",
+          body: JSON.stringify(response),
+        });
+        
+        const result = await verify.json();
+
+        if (result.success) {
+          alert("Payment Successful ");
+        } else {
+          alert("Payment Failed ");
+        }
+      },
+      theme: { color: "#4f46e5" },
+    };
+    // console.log("KEY:", process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID);
+
+
+    const rzp = new (window as any).Razorpay(options);
+    rzp.open();
+  };
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="min-h-screen flex items-center justify-center gap-6 bg-gray-100">
+      {courses.map((course, i) => (
+        <div
+          key={i}
+          className="bg-white p-6 rounded-2xl shadow-lg w-64 text-center"
+        >
+          <h2 className="text-xl font-bold">{course.name}</h2>
+          <p className="text-2xl my-4">₹{course.price}</p>
+          <p className="text-xl my-2">{course.features}</p>
+
+          <button
+            onClick={() => handlePayment(course.price)}
+            className="bg-indigo-600 text-white px-4 py-2 rounded-lg"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            Buy Now
+          </button>
         </div>
-      </main>
+      ))}
     </div>
   );
 }
